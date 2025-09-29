e7fa65844b2bcd219bc969342ad097775018ec8b
S0627 <S0627@22S0627>
2025-09-29 14:28:33 +0900

    aaa

---
+++ GameSale/src/main/java/com/example/demo/GameSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSaleApplication.class, args);
+	}
+
+}
